3d332b889bb0c9d857fd79be510d22e71202044b
kaianmuller <kaianmuller100@gmail.com>
2021-12-27 23:11:31 -0300

    Creacion de los metodos de autenticacion en AuthService.

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Login } from 'src/app/shared/models/Login.model';
+import { Utils } from 'src/app/shared/Utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http:HttpClient,private router:Router) { }
+
+
+
+  async login(login: Login){
+    return await this.http
+      .post(Utils.ip() + '/v1/login', login)
+      .toPromise()
+      .then((result) => this.storage(result));
+  }
+
+
+
+  storage(result: any):void{
+    localStorage.setItem('token', result);
+    this.router.navigate(['/calificaciones']);
+  }
+
+
+
+  getToken(){
+    return localStorage.getItem('token');
+  }
+
+
+  isLogged(): boolean {
+    return !!this.getToken();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/home']);
+  }
+
+  
 }
